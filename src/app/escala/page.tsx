b8c0983ef3d9e12a5373427b0d4a2a9c084df66f
emergencista <ba.andrade.64@gmail.com>
2026-04-02 00:06:32 +0000

    fix: render absences on escala route

--- src/app/escala/page.tsx
+++ src/app/escala/page.tsx
@@ -1,21 +1,5 @@
-"use client";
+import AbsencesPage from "./absences/page";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
-export default function EscalaRedirect() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/escala/absences");
-  }, [router]);
-
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-50">
-      <div className="text-center">
-        <div className="mb-4 inline-block h-8 w-8 animate-spin rounded-full border-4 border-gray-200 border-t-blue-600"></div>
-        <p className="text-gray-600">Redirecionando...</p>
-      </div>
-    </div>
-  );
+export default function EscalaHome() {
+  return <AbsencesPage />;
 }
